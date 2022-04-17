ff184ae3893ee4f9c098ee9384f8c8221c4a8736
Artem Lavrukhin <lavryha4590@gmail.com>
2022-04-17 18:30:35 +0300

    Fix setup py

--- setup.py
+++ setup.py
@@ -2,14 +2,18 @@
 
 from setuptools import setup, find_packages
 
-
-with open('requirements.txt', encoding='utf8') as outfile:
-    REQUIREMENTS_LIST = outfile.read().splitlines()
-
-
 setup(
     name='meeseeks-happy-birthder',
     version='1.0.0',
     packages=find_packages(),
-    install_requires=REQUIREMENTS_LIST,
+    install_requires=[
+        'aiohttp==3.8.1',
+        'alembic==1.7.5',
+        'APScheduler==3.8.1',
+        'configparser==5.2.0',
+        'gino==1.0.1',
+        'python-dotenv==0.19.1',
+        'psycopg2-binary==2.9.3',
+        'tabulate==0.8.9',
+    ],
 )
